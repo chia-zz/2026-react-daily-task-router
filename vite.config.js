4f91d7d444a42ae97614b147d036ea27b6f2e08d
Chia <angel6160@gmail.com>
2026-02-11 12:12:41 +0800

    day 43

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:
+    process.env.NODE_ENV === 'production'
+      ? '/2026-react-daily-task-router/'
+      : '/',
+
   plugins: [react()],
-})
+});
